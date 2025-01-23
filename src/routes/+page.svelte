dc06fdf969d0e309d4e85d9f75cee66358f40183
tuna <emailtolearncode@gmail.com>
2025-01-23 18:12:11 +0700

    add pokedex route

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+  import { buttonVariants } from '$lib/components/ui/button';
+</script>
+
+<a href="/pokedex" class={buttonVariants()}>Pokedex</a>
---
+++ src/routes/pokedex/+page.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  import Button from '$lib/components/ui/button/button.svelte';
+  import PokedexLogo from './icons/PokedexLogo.svelte';
+  import SearchIcon from './icons/SearchIcon.svelte';
+  import * as Select from '$lib/components/ui/select';
+
+  let sort = $state('lowest');
+</script>
+
+<main class="bg-slate-200 min-h-screen p-8">
+  <PokedexLogo />
+
+  <div
+    class="flex gap-4 items-center mt-8 bg-white py-2 px-4 rounded-lg shadow-md"
+  >
+    <SearchIcon />
+    <input type="text" placeholder="Pokemon name, number or type" />
+    <Button class="ml-auto">Search</Button>
+  </div>
+
+  <Select.Root type="single" bind:value={sort}>
+    <Select.Trigger class="w-[200px] mt-4">
+      {sort === 'lowest' ? 'Lowest Number First' : 'Highest Number First'}
+    </Select.Trigger>
+    <Select.Content>
+      <Select.Item value="lowest">Lowest Number First</Select.Item>
+      <Select.Item value="highest">Highest Number First</Select.Item>
+    </Select.Content>
+  </Select.Root>
+</main>
